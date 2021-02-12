bebc02615ff0dcbe81a0252fa3e6f95d052390fd
Tyler Boright <tyler@tylerboright.com>
2021-02-12 15:03:43 +1000

    updates the package name to be taskmao instead of taskmaster

--- src/data.rs
+++ src/data.rs
@@ -7,14 +7,20 @@ pub struct TaskDto {
     pub end_time: String,
     pub description: String,
     pub project_name: String,
+    pub running: String,
     pub start_time: String,
 }
 
 impl TaskDto {
+    pub fn end_task(&mut self, current_time: String) {
+        self.running = "false".to_string();
+        self.end_time = current_time;
+    }
+
     pub fn save_to_db (&self, conn: &Connection) -> Result<()> {
         conn.execute(
-            "INSERT INTO tasks (end_time, description, project_name, start_time) VALUES (?1, ?2, ?3, ?4)",
-            params![self.end_time, self.description, self.project_name, self.start_time],
+            "INSERT INTO tasks (end_time, description, project_name, running, start_time) VALUES (?1, ?2, ?3, ?4, ?5)",
+            params![self.end_time, self.description, self.project_name, self.running, self.start_time],
         )?;
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ mod terror;
 use chrono::prelude::*;
 use data::*;
 use clap::clap_app;
-use rusqlite::{params, Connection, Result};
-use std::collections::HashMap;
+use rusqlite::{Connection, Result};
 use std::fs;
 use std::path::PathBuf;
 use terror::*;
@@ -20,23 +19,19 @@ struct Task {
     end_time: DateTime<FixedOffset>,
     description: String,
     project_name: String,
+    running: bool,
     start_time: DateTime<FixedOffset>,
 }
 
-#[derive(Debug)]
-struct Setting {
-    key: String,
-    value: String,
-}
-
-fn get_most_recent_task(conn: &Connection, current_time: &String) -> Result<TaskDto, TError> {
-    let stmt = "SELECT description, project_name, start_time, end_time FROM tasks WHERE id = (SELECT MAX(id) FROM tasks)";
+fn get_most_recent_task(conn: &Connection) -> Result<TaskDto, TError> {
+    let stmt = "SELECT description, project_name, running, end_time, start_time FROM tasks WHERE id = (SELECT MAX(id) FROM tasks)";
     let task: TaskDto = conn.query_row(stmt, [], |r| {
         Ok(TaskDto {
             description: r.get(0)?,
             project_name: r.get(1)?,
-            start_time: r.get(2)?,
-            end_time: current_time.clone(),
+            running: r.get(2)?,
+            end_time: r.get(3)?,
+            start_time: r.get(4)?,
         })
     })?;
 
@@ -44,40 +39,27 @@ fn get_most_recent_task(conn: &Connection, current_time: &String) -> Result<Task
 }
 
 fn parse_args() -> clap::ArgMatches {
-    let matches = clap_app!(myapp =>
+    let matches = clap_app!(taskmao =>
         (version: "1.0")
         (author: "Tyler B. <tyler@tylerboright.com>")
-        (about: "The brain gains power through noticing.  Notice how you spend your time.")
-        (@arg DESC: +required "Sets the description of a task to execute")
+        (about: "Gain power through noticing.  Notice how you spend your time.")
+        (@arg DESC: "Sets the description of a task to execute")
         (@arg PROJECT: -p --project +takes_value "Sets the project of the task")
-       ).get_matches();
+        (@subcommand end =>
+            (about: "ends currently executing task")
+       )).get_matches();
 
     matches
 }
 
-fn set_up_sqlite(conn: &Connection) -> Result<Vec<Setting>, TError> {
+fn set_up_sqlite(conn: &Connection) -> Result<()> {
     let create_sql = r"
-        CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, start_time TEXT, end_time TEXT, project_name TEXT, description TEXT);
-        CREATE TABLE IF NOT EXISTS settings (key TEXT, value TEXT);
+        CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, start_time TEXT, end_time TEXT, project_name TEXT, running TEXT, description TEXT);
         ";
 
     conn.execute_batch(create_sql)?;
 
-    let mut stmt = conn.prepare("SELECT key, value FROM settings")?;
-    let rows = stmt.query_map([], |row| {
-        Ok(Setting {
-            key: row.get(0)?,
-            value: row.get(1)?,
-        })
-    })?;
-
-    let mut settings = Vec::new();
-
-    for setting in rows {
-        settings.push(setting.unwrap());
-    }
-
-    Ok(settings)
+    Ok(())
 }
 
 fn run(args: clap::ArgMatches) -> TResult<()> {
@@ -87,7 +69,7 @@ fn run(args: clap::ArgMatches) -> TResult<()> {
         None => PathBuf::from(""),
     };
 
-    path.push(".taskmaster");
+    path.push(".taskmao");
 
     fs::create_dir_all(path.as_path())?;
 
@@ -98,82 +80,65 @@ fn run(args: clap::ArgMatches) -> TResult<()> {
         Err(e) => panic!("The sqlite connection couldn't be opened with the following error: {}", e),
     };
 
-    let settings = match set_up_sqlite(&conn) {
-        Ok(settings) => {
-            let mut map: HashMap<String, String> = HashMap::new();
-            for setting in settings.iter() {
-                map.insert(
-                    setting.key.clone(),
-                    setting.value.clone(),
-                );
-            }
-
-            map
+    match set_up_sqlite(&conn) {
+        Ok(_val) => {
         },
         Err(err) => {
-            println!("The settings for the application couldn't be parsed with the following error: {}", err);
-            HashMap::new()
+            println!("The data store for tasks could not be set up with the following error: {}", err);
         },
     };
 
-    let is_initial_setup: bool = match settings.len() {
-        0 => true,
-        _ => false,
-    };
+    match args.subcommand_name() {
+        Some("end") => { 
+            println!("{}", "end task"); 
+        },
+        None => {
+            let project: String = match args.value_of("PROJECT") {
+                Some(proj) => proj.to_string(),
+                None => "default".to_string(),
+            };
 
-    if is_initial_setup {
-        conn.execute(
-            "INSERT INTO settings (key, value) VALUES (?1, ?2)",
-            params!["running", "false"],
-        )?;
-    }
-
-    let project: String = match args.value_of("PROJECT") {
-        Some(proj) => proj.to_string(),
-        None => "default".to_string(),
-    };
-
-    if let Some(n) = args.value_of("DESC") {
-        let running = match settings.get("running") {
-            Some(val) => val,
-            _ => "false",
-        };
-
-        match running {
-            "true" => { 
+            if let Some(n) = args.value_of("DESC") {
                 // grab most recent entry from sqlite
                 let current_time = Local::now().to_rfc3339();
-                let prev_task = get_most_recent_task(&conn, &current_time)?;
-                println!("{:?}", prev_task);
 
                 let new_task = TaskDto {
                     end_time: current_time.clone(),
                     description: n.to_string(),
-                    project_name: project,
+                    project_name: project.clone(),
+                    running: "true".to_string(),
                     start_time: current_time.clone(),
                 };
 
-                prev_task.save_to_db(&conn)?;
-                new_task.save_to_db(&conn)?;
-            },
-            "false" => { 
-                let current_time = Local::now().to_rfc3339();
-                let new_task = TaskDto {
-                    end_time: current_time.clone(),
-                    description: n.to_string(),
-                    project_name: project,
-                    start_time: current_time.clone(),
+                let mut prev_task = match get_most_recent_task(&conn) {
+                    Ok(task) => task,
+                    Err(_err) => {
+                        TaskDto {
+                            end_time: current_time.clone(),
+                            description: n.to_string(),
+                            project_name: project,
+                            running: "false".to_string(),
+                            start_time: current_time.clone(),
+                        }
+                    }
                 };
-                
-                new_task.save_to_db(&conn);
-                conn.execute(
-                    "UPDATE settings SET value = 'true' WHERE key = 'running'",
-                    params![],
-                )?;
-            },
-            _ => (),
-        }
-    }
+                match &prev_task.running[..] {
+                    "true" => { 
+                        prev_task.end_task(current_time);
+                        prev_task.save_to_db(&conn)?;
+                        new_task.save_to_db(&conn)?;
+                    },
+                    _ => {
+                        new_task.save_to_db(&conn)?;
+                    },
+                }
+
+                println!("started running {} at {}", new_task.description, new_task.start_time);
+            }
+
+        },
+        _ => println!("{}", "taskmao: try 'taskmao --help' for more information"),
+    };
 
     Ok(())
 }
